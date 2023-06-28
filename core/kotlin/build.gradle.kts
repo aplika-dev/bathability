80318029dd11cc9bf2eae36203bcc6875fdb3b7c
Frederico Gassen <fredericogassen@gmail.com>
2023-06-28 16:10:57 -0300

    add kotlin module

---
+++ core/kotlin/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id(libs.plugins.javaLibrary.get().pluginId)
+    alias(libs.plugins.kotlinJvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
