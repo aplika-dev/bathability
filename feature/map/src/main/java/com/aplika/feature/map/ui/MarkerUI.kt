1b8f8d95e0a43a1fc8f2451824268dfdb1f9fef3
Frederico Gassen <fredericogassen@gmail.com>
2023-06-30 15:56:56 -0300

    Update MarkerUI.kt

--- feature/map/src/main/java/com/aplika/feature/map/ui/MarkerUI.kt
+++ feature/map/src/main/java/com/aplika/feature/map/ui/MarkerUI.kt
@@ -1,11 +1,13 @@
 package com.aplika.feature.map.ui
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.aplika.feature.map.presentation.MarkerPresentation
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerInfoWindow
 import com.google.maps.android.compose.rememberMarkerState
 
@@ -13,10 +15,12 @@ import com.google.maps.android.compose.rememberMarkerState
 internal fun MarkerUI(
     marker: MarkerPresentation
 ) {
-    MarkerInfoWindow(
+    Marker(
         state = rememberMarkerState(position = marker.position),
-        icon = BitmapDescriptorFactory.defaultMarker(marker.color)
-    ) {
-        MarkerWindowUI(markerInfoPresentation = marker.info)
-    }
+        icon = BitmapDescriptorFactory.defaultMarker(marker.color),
+        onClick = {
+            Log.d("xxx", "test")
+            true
+        }
+    )
 }
