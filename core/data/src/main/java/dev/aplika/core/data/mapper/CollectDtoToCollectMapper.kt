5c41d459375641cb769c1e25699ae8664d89e829
Frederico Gassen <fredericogassen@gmail.com>
2023-11-14 21:28:36 -0300

    Update CollectDtoToCollectMapper.kt (#6)

--- core/data/src/main/java/dev/aplika/core/data/mapper/CollectDtoToCollectMapper.kt
+++ core/data/src/main/java/dev/aplika/core/data/mapper/CollectDtoToCollectMapper.kt
@@ -8,14 +8,16 @@ import dev.aplika.core.network.model.CollectDto
 import java.text.SimpleDateFormat
 import java.util.Locale
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 class CollectDtoToCollectMapper @Inject constructor() : Mapper<CollectDto, Collect?> {
     override fun map(input: CollectDto): Collect? {
+        val escherichiaColiFactor = input.escherichiaColiFactor.toFloatOrNull()?.roundToInt() ?: return null
         return Collect(
             date = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")).parse(input.date) ?: return null,
             bathabilitySituation = BathabilitySituation.getById(id = input.bathabilitySituation),
             rainSituation = RainSituation.getById(id = input.rainSituation),
-            escherichiaColiFactor = input.escherichiaColiFactor.toInt()
+            escherichiaColiFactor = escherichiaColiFactor
         )
     }
 }
