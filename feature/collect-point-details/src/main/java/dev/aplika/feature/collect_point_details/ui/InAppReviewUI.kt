3793e412d97d38a6c2253039675cd3d492efca93
Frederico Gassen <fredericogassen@gmail.com>
2024-01-27 21:58:28 -0300

    Update InAppReviewUI.kt

--- feature/collect-point-details/src/main/java/dev/aplika/feature/collect_point_details/ui/InAppReviewUI.kt
+++ feature/collect-point-details/src/main/java/dev/aplika/feature/collect_point_details/ui/InAppReviewUI.kt
@@ -1,6 +1,7 @@
 package dev.aplika.feature.collect_point_details.ui
 
 import android.app.Activity
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -8,36 +9,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import com.google.android.play.core.ktx.launchReview
+import com.google.android.play.core.ktx.requestReview
 import com.google.android.play.core.review.ReviewInfo
 import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
+import kotlinx.coroutines.delay
+
+private const val DELAY_TO_REQUEST_REVIEW_IN_MILLIS = 5000L
 
 @Composable
 internal fun InAppReviewUI() {
-    val localContext = LocalContext.current
+    val context = LocalContext.current
 
-    val reviewManager = remember {
-        ReviewManagerFactory.create(localContext)
+    val reviewManager = remember { ReviewManagerFactory.create(context) }
+
+    LaunchedEffect(key1 = reviewManager) {
+        delay(DELAY_TO_REQUEST_REVIEW_IN_MILLIS)
+        val reviewInfo = reviewManager.requestReview()
+        reviewManager.launchReview(context as Activity, reviewInfo)
     }
-
-    val reviewInfo = rememberReviewTask(reviewManager)
-
-    LaunchedEffect(key1 = reviewInfo) {
-        reviewInfo?.let {
-            reviewManager.launchReviewFlow(localContext as Activity, reviewInfo)
-        }
-    }
-}
-
-@Composable
-fun rememberReviewTask(reviewManager: ReviewManager): ReviewInfo? {
-    var reviewInfo: ReviewInfo? by remember { mutableStateOf(null) }
-
-    reviewManager.requestReviewFlow().addOnCompleteListener {
-        if (it.isSuccessful) {
-            reviewInfo = it.result
-        }
-    }
-
-    return reviewInfo
 }
